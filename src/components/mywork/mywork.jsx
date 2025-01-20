382b82a3dbeb4a2ecf96d7037291bb885567129f
Vimukthiaravinda <vimukthiaravinda18@gmail.com>
2025-01-21 00:35:41 +0530

    deploy react_4

--- src/components/mywork/mywork.jsx
+++ src/components/mywork/mywork.jsx
@@ -1,7 +1,33 @@
 import 'react';
 import './mywork.css'
-import mywork_data from '../../assets/work_data.json';
+import portfolio from "../../assets/portfolio.png";
+import Cimel from "../../assets/cimel.png";
+import Xess from "../../assets/xess.png";
+import Leapy from "../../assets/leapy.png";
+
 const Mywork = () => {
+    const mywork_data = [
+        {
+            work_no: "1",
+            work_name: "Portfolio",
+            work_image: portfolio
+        },
+        {
+            work_no: "2",
+            work_name: "Cimel Engineering",
+            work_image: Cimel
+        },
+        {
+            work_no: "3",
+            work_name: "Xess Global",
+            work_image: Xess
+        },
+        {
+            work_no: "4",
+            work_name: "Leapy",
+            work_image: Leapy
+        }
+    ]
     return (
         <div id = 'projects' className="mywork">
             <div className="mywork-title">
